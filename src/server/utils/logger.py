2ff62b7dd887dd51937e4beaa0b8e196c84781bd
Lotuxx <s.diop@myskolae.fr>
2026-04-30 20:26:08 +0200

    update srv/logger.py

--- src/server/utils/logger.py
+++ src/server/utils/logger.py
@@ -1,10 +1,8 @@
-# logger.py
-
 import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format="[% (asctime)s] %(levelname)s - %(message)s",
+    format="[%(asctime)s] [%(levelname)s] %(message)s",
     datefmt="%H:%M:%S"
 )
 
